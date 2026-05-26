4285ff3d9a4dc3e9a226e6c04ffd2aabeba15a27
Benexl <benextempest@gmail.com>
2026-05-26 17:16:03 +0300

    feat: ui_launcher def

--- shelx/ui_launcher.sh
+++ shelx/ui_launcher.sh
@@ -1,72 +1,134 @@
-# ==============================================================================
-# CORE UI: notify
-# ==============================================================================
-__ui_notify_terminal() {
-  #TODO: finish implementation
-  case "$1" in
-  info) printf '%s\n' "$2" >&2 && sleep "$CONFIG_NOTIFICATION_DURATION" ;;
-  warning) printf '%s\n' "$2" >&2 && sleep "$CONFIG_NOTIFICATION_DURATION" ;;
-  error) printf '%s\n' "$2" >&2 && sleep "$CONFIG_NOTIFICATION_DURATION" ;;
-  critical) printf '%s\n' "$2" >&2 && exit 1 ;;
-  *) printf '%s\n' "$2" >&2 && sleep "$CONFIG_NOTIFICATION_DURATION" ;;
+__ui_gum_launcher() {
+  local custom_opts
+  local multi_select
+  multi_select="$2"
+
+  [ "$multi_select" = "multi" ] && custom_opts="--no-limit"
+  [ -n "$CONFIG_FZF_HEADER" ] && custom_opts="$custom_opts --header $CONFIG_FZF_HEADER"
+
+  local old_ifs
+  old_ifs="$IFS"
+  IFS=" "
+
+  # shellcheck disable=SC2086
+  gum filter $CONFIG_GUM_FILTER_OPTS --prompt="${1}: " $custom_opts
+
+  IFS="$old_ifs"
+}
+
+__ui_fzf_launcher() {
+  local custom_opts
+  local multi_select
+  multi_select="$2"
+
+  [ "$multi_select" = "multi" ] && custom_opts="--multi"
+  [ -n "$CONFIG_FZF_HEADER" ] && custom_opts="$custom_opts --header-first --header=$CONFIG_FZF_HEADER"
+
+  # TODO: is there a better way to do this
+  local old_ifs
+  old_ifs="$IFS"
+  IFS=" "
+
+  # shellcheck disable=SC2086
+  fzf --prompt="${1}: " $custom_opts
+
+  IFS="$old_ifs"
+}
+
+__ui_rofi_launcher() {
+  ! [ -s "$CONFIG_ROFI_THEME_MAIN" ] && ui_notify_critical "$TXT_ROFI_NOT_CONFIGURED: where CONFIG_ROFI_THEME_MAIN=\"$CONFIG_ROFI_THEME_MAIN\""
+  local selection
+  local custom_opts
+  local multi_select
+  local line
+
+  multi_select="$2"
+  [ "$multi_select" = "multi" ] && custom_opts="-multi-select"
+
+  # shellcheck disable=SC2086
+  selection=$(
+    sed -E "s/$THEME_ESC(\[[0-9;]*[a-zA-Z]|\(B)//g" |
+      rofi -no-config -theme "$CONFIG_ROFI_THEME_MAIN" -dmenu -i $custom_opts -p "$1"
+  )
+  printf '%s' "$selection"
+}
+
+_ui_launcher() {
+  case "$CONFIG_LAUNCHER" in
+  rofi) __ui_rofi_launcher "$@" ;;
+  fzf) __ui_fzf_launcher "$@" ;;
+  gum) __ui_gum_launcher "$@" ;;
+  *) ui_notify_critical "$TXT_LAUNCHER_UNKNOWN: $CONFIG_LAUNCHER" ;;
   esac
-
 }
 
-__ui_notify_non_terminal() {
-  #TODO: finish implementation
-  case "$1" in
-  info) notify-send "$2" ;;
-  warning) notify-send "$2" ;;
-  error) notify-send "$2" ;;
-  critical) notify-send "$2" && exit 1 ;;
-  esac
+ui_launcher() {
+  _ui_launcher "$@"
 }
 
-_ui_notify() {
-  if $CLI_IS_TERMINAL; then
-    __ui_notify_terminal info "$1"
+# ==============================================================================
+# CORE UI: launcher with preview
+# ==============================================================================
+__ui_fzf_launcher_with_preview() {
+  local custom_opts
+  local multi_select
+  local preview_script
+
+  preview_script="\
+TXT_PREVIEW_INSTALL_VIEWER='$TXT_PREVIEW_INSTALL_VIEWER'
+CONFIG_IMAGE_RENDERER='$CONFIG_IMAGE_RENDERER'
+CLI_PLATFORM='$CLI_PLATFORM'
+THEME_FZF_PREVIEW_DIVIDER='$THEME_FZF_PREVIEW_DIVIDER'
+preview_script_path='{1}'
+[ -s \"\$preview_script_path\" ] && . \"\$preview_script_path\"\
+"
+
+  multi_select="$2"
+  [ "$multi_select" = "multi" ] && custom_opts="--multi"
+  [ -n "$CONFIG_FZF_HEADER" ] && custom_opts="$custom_opts --header-first --header=$CONFIG_FZF_HEADER"
+
+  # TODO: is there a better way to do this
+  local old_ifs
+  old_ifs="$IFS"
+  IFS=" "
+
+  # shellcheck disable=SC2086
+  fzf --prompt="${1}: " --delimiter '|' --with-nth "{2..}" --accept-nth "{2..}" --preview="$preview_script" $custom_opts
+
+  IFS="$old_ifs"
+}
+
+__ui_rofi_launcher_with_preview() {
+  ! [ -s "$CONFIG_ROFI_THEME_PREVIEW" ] && ui_notify_critical "$TXT_ROFI_NOT_CONFIGURED: where CONFIG_ROFI_THEME_PREVIEW=\"$CONFIG_ROFI_THEME_PREVIEW\""
+  local selection
+  local custom_opts
+  local multi_select
+  local line
+
+  multi_select="$2"
+  [ "$multi_select" = "multi" ] && custom_opts="-multi-select"
+
+  # shellcheck disable=SC2086
+  selection=$(
+    sed -E "s/$THEME_ESC(\[[0-9;]*[a-zA-Z]|\(B)//g" |
+      rofi -no-config -theme "$CONFIG_ROFI_THEME_PREVIEW" -dmenu -i $custom_opts -p "$1"
+  )
+  printf '%s' "$selection"
+}
+
+_ui_launcher_with_preview() {
+  if [ "$CONFIG_ENABLE_PREVIEW" = "true" ]; then
+    case "$CONFIG_LAUNCHER" in
+    rofi) __ui_rofi_launcher_with_preview "$@" ;;
+    fzf) __ui_fzf_launcher_with_preview "$@" ;;
+    gum) __ui_gum_launcher "$@" ;;
+    *) ui_notify_critical "$TXT_LAUNCHER_UNKNOWN: $CONFIG_LAUNCHER" ;;
+    esac
   else
-    __ui_notify_non_terminal info "$1"
+    ui_launcher "$@" && return
   fi
 }
 
-_ui_notify_warning() {
-  if $CLI_IS_TERMINAL; then
-    __ui_notify_terminal warning "$1"
-  else
-    __ui_notify_non_terminal warning "$1"
-  fi
-}
-
-_ui_notify_error() {
-  if $CLI_IS_TERMINAL; then
-    __ui_notify_terminal error "$1"
-  else
-    __ui_notify_non_terminal error "$1"
-  fi
-}
-
-_ui_notify_critical() {
-  if $CLI_IS_TERMINAL; then
-    __ui_notify_terminal critical "$1"
-  else
-    __ui_notify_non_terminal critical "$1"
-  fi
-}
-
-ui_notify() {
-  _ui_notify "$@"
-}
-
-ui_notify_warning() {
-  _ui_notify_warning "$@"
-}
-
-ui_notify_error() {
-  _ui_notify_error "$@"
-}
-
-ui_notify_critical() {
-  _ui_notify_critical "$@"
+ui_launcher_with_preview() {
+  _ui_launcher_with_preview "$@"
 }
